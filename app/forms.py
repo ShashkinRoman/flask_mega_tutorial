817cfa9891e6b3701f1aecfcdaf4fdbe8ab1c558
Roman Shashkin <romanshashkin@mail.ru>
2019-08-28 13:09:47 +0400

    added commends for third lesson. fourth lesson in progres

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,24 @@
-from flask_wtf import FlaskForm
+'''Расширение Flask-WTF использует классы Python для представления веб-форм.
+Класс формы просто определяет поля формы как переменные класса.
+
+Еще раз имея в виду разделение проблем, я собираюсь использовать новый
+app/forms.py модуль для хранения классов веб-форм.'''
+
+
+from flask_wtf import FlaskForm #для обработки вебформ, обертка WTForms for Flask
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
+# Четыре класса, которые представляют типы полей, которые я использую для этой
+# формы, импортируются непосредственно из пакета WTForms, поскольку расширение
+# Flask-WTF не предоставляет настраиваемые версии. Для каждого поля объект
+# создается как переменная класса в классе LoginForm. Каждому полю присваивается
+# описание или метка в качестве первого аргумента.
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
+    # Валидатор DataRequired - проверяет что поле не отправлено пустым
+    # есть разные валидаторы
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
--- app/routes.py
+++ app/routes.py
@@ -32,11 +32,24 @@ def index():
     # указанными в вызове render_template().
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST']) # По факту добавляет
+# часть ссылки, для перехода на страницу указанной в функции
+# methods - объясняет, что фунция принимает эти запросы, а не только get
 def login():
     form = LoginForm()
     if form.validate_on_submit():
+        # Метод form.validate_on_submit() выполняет всю обработку формы.
+        # Когда браузер отправляет запрос GET для получения веб-страницы с
+        # формой, этот метод возвращает False, поэтому в этом случае функция
+        # пропускает оператор if и переходит к отображению шаблона в последней
+        # строке функции.
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
+# Когда браузер отправляет запрос POST в результате нажатия пользователем кнопки
+# submit, form.validate_on_submit() собирает все данные, запускает все валидаторы,
+# прикрепленные к полям, и если все в порядке, вернет True, сообща что данные
+# действительны и могут быть обработаны приложением. Но если хотя бы одно поле не
+# подтвердит проверку, функция вернет False, и это приведет к тому, что форма будет
+# возвращена пользователю, например, в случае запроса GET.
